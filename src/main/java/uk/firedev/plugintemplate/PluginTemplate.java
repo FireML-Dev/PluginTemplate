4d200c6c9c162d7ad9e60cb4d65b0157717466f9
FireML <fireml@firedev.uk>
2025-10-31 00:32:51 +0000

    Add registerCommands method to main class

--- src/main/java/uk/firedev/plugintemplate/PluginTemplate.java
+++ src/main/java/uk/firedev/plugintemplate/PluginTemplate.java
@@ -1,6 +1,8 @@
 package uk.firedev.plugintemplate;
 
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class PluginTemplate extends JavaPlugin {
 
@@ -28,5 +30,12 @@ public final class PluginTemplate extends JavaPlugin {
 
     @Override
     public void onDisable() {}
+
+    private void registerCommands() {
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            // Register Brigadier commands here.
+            // commands.registrar().register(new MyCommand().get());
+        });
+    }
 
 }
