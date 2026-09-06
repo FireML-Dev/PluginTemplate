4cb2bd9e80e751d5e29e8111a34139e0b87eb5fa
FireML <fireml@firedev.uk>
2026-09-06 01:14:21 +0100

    Switch to libs.versions.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,1 @@
 rootProject.name = "PluginTemplate"
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            // compileOnly dependencies
-            library("paper-api", "io.papermc.paper:paper-api:26.2.build.+")
-
-            // implementation dependencies
-
-            // paperLibrary dependencies
-            library("daisylib", "uk.firedev:DaisyLib:4.0-SNAPSHOT")
-
-            // Gradle plugins
-            plugin("shadow", "com.gradleup.shadow").version("9.6.1")
-            plugin("plugin-yml", "de.eldoria.plugin-yml.paper").version("0.9.0")
-        }
-    }
-}
